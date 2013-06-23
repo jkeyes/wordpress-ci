335df57868646b1b6e76c5bb2e6affc9d44b38b2
John Keyes <johnkeyes@gmail.com>
2013-06-23 21:27:50 +0100

    Try to connect to MySQL via host + port.

--- sample.php
+++ sample.php
@@ -1,3 +1,12 @@
 <?php
 echo "Werckered!"
+
+// we connect to example.com and port 3307
+$link = mysql_connect($_ENV['WERCKER_MYSQL_HOST'] . ':' . $_ENV['WERCKER_MYSQL_PORT'], $_ENV['WERCKER_MYSQL_USERNAME'], $_ENV['WERCKER_MYSQL_PASSWORD']);
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+mysql_close($link);
+
 ?>
