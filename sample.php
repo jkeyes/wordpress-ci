b394294e22a698ff29d5d9a893916f084e55de1c
John Keyes <johnkeyes@gmail.com>
2013-06-23 21:37:48 +0100

    Fix syntax error.

--- sample.php
+++ sample.php
@@ -1,5 +1,5 @@
 <?php
-echo "Werckered!"
+echo "Werckered!";
 
 // we connect to example.com and port 3307
 $link = mysql_connect($_ENV['WERCKER_MYSQL_HOST'] . ':' . $_ENV['WERCKER_MYSQL_PORT'], $_ENV['WERCKER_MYSQL_USERNAME'], $_ENV['WERCKER_MYSQL_PASSWORD']);
