6d5744ee3e76be510a4c5f67616c44da77b2b606
John Keyes <johnkeyes@gmail.com>
2013-06-23 17:45:55 +0100

    Sudo the move to the docroot.

--- configure_wordpress.sh
+++ configure_wordpress.sh
@@ -5,7 +5,7 @@ SITE_ROOT="/var/www/"
 
 # extract the WordPress archive
 tar zxf wordpress-3.5.2.tar.gz
-mv wordpress/* $SITE_ROOT
+sudo mv wordpress/* $SITE_ROOT
 
 # create the database
 mysql -u $WERCKER_MYSQL_USERNAME --password=$WERCKER_MYSQL_PASSWORD \
@@ -14,7 +14,7 @@ mysql -u $WERCKER_MYSQL_USERNAME --password=$WERCKER_MYSQL_PASSWORD \
     -e "GRANT ALL PRIVILEGES ON site.* TO $WERCKER_MYSQL_USERNAME@$WERCKER_MYSQL_HOST IDENTIFIED BY '$WERCKER_MYSQL_PASSWORD'"
 
 # create the wp-config file
-sed -e "s/{{DB_NAME}}/$DB_NAME/" \
+sudo sed -e "s/{{DB_NAME}}/$DB_NAME/" \
     -e "s/{{DB_USER}}/$WERCKER_MYSQL_USERNAME/" \
     -e "s/{{DB_PASSWORD}}/$WERCKER_MYSQL_PASSWORD/" \
     -e "s/{{DB_HOST}}/$WERCKER_MYSQL_HOST:$WERCKER_MYSQL_PORT/" \
