265bad1229f60fa81c6e7b5d00e103d88357afe0
John Keyes <johnkeyes@gmail.com>
2013-06-23 18:18:48 +0100

    Move the config file.

--- configure_wordpress.sh
+++ configure_wordpress.sh
@@ -14,8 +14,10 @@ mysql -u $WERCKER_MYSQL_USERNAME --password=$WERCKER_MYSQL_PASSWORD \
     --port $WERCKER_MYSQL_PORT -e "GRANT ALL PRIVILEGES ON site.* TO $WERCKER_MYSQL_USERNAME@$WERCKER_MYSQL_HOST IDENTIFIED BY '$WERCKER_MYSQL_PASSWORD'"
 
 # create the wp-config file
-sudo sed -e "s/{{DB_NAME}}/$DB_NAME/" \
+sed -e "s/{{DB_NAME}}/$DB_NAME/" \
     -e "s/{{DB_USER}}/$WERCKER_MYSQL_USERNAME/" \
     -e "s/{{DB_PASSWORD}}/$WERCKER_MYSQL_PASSWORD/" \
     -e "s/{{DB_HOST}}/$WERCKER_MYSQL_HOST:$WERCKER_MYSQL_PORT/" \
- < wp-config.template > $SITE_ROOT/wp-config.php
+ < wp-config.template > wp-config.php
+
+ sudo mv wp-config.php $SITE_ROOT/wp-config.php
